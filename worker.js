377440d20745f6bd0b03bd5d63a4edf5fd91a563
Avishay Asraf <avishayasraf123@gmail.com>
2026-09-06 20:17:45 +0300

    fix: worker base_url must start with https://, fix features white screen

--- worker.js
+++ worker.js
@@ -246,7 +246,7 @@ async function handleForgotPassword(request, env) {
     );
 
     // קישור איפוס
-    const resetLink = (base_url && !base_url.startsWith('file://') ? base_url : 'https://auditera.co/reset.html') + '?reset=' + token;
+    const resetLink = (base_url && base_url.startsWith('https://') ? base_url : 'https://auditera.co/reset.html') + '?reset=' + token;
 
     // שליחה ל-Make
     if (env.MAKE_WEBHOOK_URL) {
